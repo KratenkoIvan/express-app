ee6ba0bb206c83354bd8724164e51d6ff4afe42a
Kratenko Ivan <kratenkovanya@gmail.com>
2024-09-24 10:19:43 +0300

    added ejs and page posts

--- src/server.js
+++ src/server.js
@@ -6,6 +6,9 @@ const app = express()
 const PORT = 8000
 const HOST = 'localhost'
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'templates'))
+
 app.use('/static/', express.static(path.join(__dirname, 'static')))
 function getDate(){
     console.log(moment().format('YYYY/DD/MM HH:mm:ss'))
@@ -19,6 +22,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './templates/index.html'))
 })
 
+app.get('/posts', (req, res) => {
+    const context = {
+        posts: [{name: 'post1', author: 'Author1 '}, {name: 'post2', author: 'Author2'}, {name: 'post3', author: 'Author3'}, {name: 'post4', author: 'Author4'}]
+    }
+
+    res.render('posts', context)
+})
 app.listen(PORT, HOST, () => {
     console.log(`Server running at http://${HOST}:${PORT}/`)
 });
